93e96371e92498025dc8fc31e0927f2afd84b34b
Luciano Frizzera <lucaju@gmail.com>
2021-06-26 14:28:08 -0400

    feat: 🎸 Add the ability to get metadata for a tag, including its attributes

--- src/index.worker.ts
+++ src/index.worker.ts
@@ -1,16 +1,17 @@
-import * as salve from 'salve-annos/build/dist';
-// import { expose } from 'threads/worker';
 import * as Comlink from 'comlink';
+import * as salve from 'salve-annos/build/dist';
 import { loadSchema, SchemaRequest } from './conversion';
 //@ts-ignore
 import jsdom from './lib/jsdom/jsdom-browserified.js';
 import { possibleAt, PossibleRequest } from './possible';
+import { attributesForTag, tagAt, TagRequest } from './tag';
 import { validate, validatePossibleAt, ValidationResponse } from './validate';
 import { virtualEditor } from './virtualEditor';
 
 export { SchemaRequest, SchemaResponse } from './conversion';
 export { PossibleRequest, PossibleResponse, Selection } from './possible';
 export { Tag } from './sharedTypes';
+export { TagAttribute, TagRequest } from './tag';
 export {
   PossibleNodes,
   ValidatePossibleAtResponse,
@@ -34,11 +35,17 @@ const cwrcWorkerValidator = {
   async loadSchema(schema: SchemaRequest) {
     return await loadSchema(schema);
   },
-  validate(documentString: string, callback: (value:ValidationResponse) => void) {
+  validate(documentString: string, callback: (value: ValidationResponse) => void) {
     return validate(documentString, callback);
   },
   async validatePossible(xpath: string, index: number, type: string) {
     return await validatePossibleAt(xpath, index, type);
+  },
+  async tagAt(tag: TagRequest) {
+    return await tagAt(tag);
+  },
+  async attributesForTag(xpath: string, index?: number) {
+    return await attributesForTag(xpath, index);
   },
   hasValidator() {
     return virtualEditor.hasValidator();
